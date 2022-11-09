364d19fa5deda33fb9e96e3f6b00f9e73c9a135a
hy <rlagpdud0524@gmail.com>
2022-11-09 20:33:34 +0900

    ,l

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask
+from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def root():
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
     return render_template('seoul_cctv.html')
 
-app.run(host='127.0.0.1', port=8080)
+if __name__ == '__main__':
+    app.debug = True
+    app.run(debug=True)
